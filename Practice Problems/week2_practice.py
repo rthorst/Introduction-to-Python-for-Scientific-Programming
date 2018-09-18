0933a56b0c8022703e91ae581e8e7d2ae98f2604
rthorst <thorstadrs@gmail.com>
2018-09-17 21:25:16 -0400

    add week 2 practice problems
    add practice problems for week 2, variable types

---
+++ Practice Problems/week2_practice.py
@@ -0,0 +1,39 @@
+"""
+-- Practice variable assignment. --
+
+Imagine that, 
+3 Participants completed a "future fluency task" where they generated as many
+future events as they could in 60 seconds.  The number of events generated was:
+    
+    participant 1  :   2 events.
+    participant 2  :   4 events.
+    participant 3  :   3 events.
+
+Store these scores as 3 separate variables.
+"""
+p1_score = 
+
+
+"""
+--- Practice variable types ---
+
+There is something wrong with the type of each variable below.
+Correct the mistake, 
+      First, by fixing the way the variable is declared.
+      E.g. (bad) score = "4.0" --> (good) score = 4.0
+      
+      Second, by using casting.
+      E.g. (bad) score = "4.0" --> (good) score = float(score)
+"""
+
+age = "15"
+gender = male
+finished_task = "fALSE"
+
+"""
+--- Practice variable operations ---
+Above you created 3 varaiables for the scores of participants 1, 2, and 3.
+Get the average of the scores (manually, using + and /)
+Store this average as a variable called M_score
+"""
+
